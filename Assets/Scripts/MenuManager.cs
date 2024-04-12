6e4ae11be778bd628890c8e2b4f42fb331ac0ab5
Prescott Stebbins <prescott.stebbins@tooeleschools.org>
2024-04-12 09:35:47 -0600

    Main Menu GUI

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    private enum sceneNames
+    {
+        CLC_Splash = 0,
+        CLC_Period = 1,
+        Main_Menu = 2,
+        Menu_Settings = 3,
+        Credits = 4,
+        World_Select = 5,
+        Plains = 6,
+        A_New_Beginning = 7,
+        On_Your_Way = 8,
+        Ramping_Up = 9,
+        Grass_Eater = 10,
+        Deep_Forest = 11,
+        Into_The_Woods = 12
+    }
+}
